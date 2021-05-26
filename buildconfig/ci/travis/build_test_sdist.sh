df1666f4578ce8fce36d904bf41b1b1da291fe96
Hirni-Meshram3 <84446123+Hirni-Meshram3@users.noreply.github.com>
2021-05-26 12:26:25 +0530

    Update build_test_sdist.sh

--- buildconfig/ci/travis/build_test_sdist.sh
+++ buildconfig/ci/travis/build_test_sdist.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e -x
-cd ../../../..
+#cd ../../../..
 $PYTHON_EXE setup.py sdist
 $PYTHON_EXE -m pip install dist/*.tar.gz
 $PYTHON_EXE -m pygame.tests.__main__ -v --exclude opengl,timing --time_out 300
